c565e4e3e9cd3b0af770a93cefb302da1a7faaf5
Aditya Verma <adichichi162@gmail.com>
2022-03-13 19:28:29 +0530

    Added a funtion file

---
+++ Func-eg.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+//A simple program to print the sum of integers using funtion.
+
+int sum(int a,int b)
+{
+    return (a+b);
+}
+int main()
+{
+    int a,b;
+    cout<<"Enter the two numbers :";
+    cin>>a>>b;
+    cout<<"Sum is : "<<sum(a,b);
+    return 0;
+}
